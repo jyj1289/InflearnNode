b0876c30177c678da3e65a8bd71d3e89ac6cb3ef
jyj1289 <jin37091289@gmail.com>
2023-07-28 14:17:39 +0900

    feat:비밀번호맞는지 확인

--- index.js
+++ index.js
@@ -1,38 +1,67 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 3000;
-const bodyParser = require('body-parser')
-const { User } = require('./models/User');
-const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const { User } = require("./models/User");
+const mongoose = require("mongoose");
 
 // application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 // application/json
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb+srv://jyj1289:abcd1234@board.abbggdo.mongodb.net/', {
-    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false
-}).then(() => console.log('MongoDB Connected...'))
-.catch(err => console.log(err));
+mongoose
+    .connect("mongodb+srv://jyj1289:abcd1234@board.abbggdo.mongodb.net/", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log("MongoDB Connected..."))
+    .catch((err) => console.log(err));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
     res.send("Hello World!");
 });
 
-app.post('/register', (req, res) => {
-
+app.post("/register", (req, res) => {
     // 회원 가입 할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
 
-    
     const user = new User(req.body);
 
     user.save((err, doc) => {
-        if(err) return res.json({success: false, err});
+        if (err) return res.json({ success: false, err });
         return res.status(200).json({
-            success: true
+            success: true,
         });
+    });
+});
+
+app.post("/login", (req, res) => {
+    // 요청된 이메일이 데이터베이스에 있는지 찾는다
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "제공된 이메일에 해당하는 유저가 없습니다.",
+            });
+        }
+    });
+
+    // 요청된 이메일이 데이터베이스에 있다면 비밀번호가 맞는 비밀번호인지 확인
+    user.comparePassword(req.body.password, (err, isMatch) => {
+        if (!isMatch)
+            return res.json({
+                loginSuccess: false,
+                message: "비밀번호가 틀렸습니다.",
+            });
+
+        // 비밀번호 까지 맞다면 토큰을 생성하기 
+        user.generateToken((err, user) => {
+            
+        });
+        
     });
 
 });
