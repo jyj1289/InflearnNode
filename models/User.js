4bbd81ff66616c9358c23710b252e9b05747fa30
jyj1289 <jin37091289@gmail.com>
2023-08-05 22:27:08 +0900

    FIX :: logout 기능 수정

--- models/User.js
+++ models/User.js
@@ -80,16 +80,16 @@ userSchema.statics.findByToken = function (token, cb) {
     let user = this;
 
     // 토큰을 decode한다
-    jwt.verify(user.token, "secretToken", function (err, decoded) {
-        // 유저 아이디를 이용해서 유저를 찾은 다음에
+    jwt.verify(token, "secretToken", function (err, decoded) {
+        if (err) return cb(err);
         // 클라이언트에서 가져온 token과 DB에 보관된 토큰이 일치하는지 확인
-
         user.findOne({ "_id": decoded, "token": token }, function (err, user) {
             if (err) return cb(err);
             cb(null, user);
         });
     });
 };
+
 const User = mongoose.model("User", userSchema);
 
 module.exports = { User };
