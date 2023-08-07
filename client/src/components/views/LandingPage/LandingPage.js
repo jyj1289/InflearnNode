eb5408ae1ee291cd699170876e4fa577faa1cd59
jyj1289 <jin37091289@gmail.com>
2023-08-07 14:24:28 +0900

    FEAT :: axios를 이용한 hello api

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -1,9 +1,11 @@
 import React, { useEffect } from "react";
 import axios from "axios";
+import { response } from "express";
 
 function LandingPage() {
     useEffect(() => {
-        axios.get("/api/hello");
+        axios.get("/api/hello")
+        .then((response) => console.log(response.data));
     }, []);
     return <div>LandingPage</div>;
 }
