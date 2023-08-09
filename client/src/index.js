bd8789cc04a8dbd13ce6ee398b3339032902045a
jyj1289 <jin37091289@gmail.com>
2023-08-09 17:57:05 +0900

    FEAT ::: Redux devtools

--- client/src/index.js
+++ client/src/index.js
@@ -8,14 +8,19 @@ import "antd";
 import { applyMiddleware, createStore } from "redux";
 import promiseMiddleware from "redux-promise";
 import ReduxThunk from "redux-thunk";
+import Reducer from "./_reducers";
 
-const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
+const createStoreWithMiddleware = applyMiddleware(
+    promiseMiddleware,
+    ReduxThunk
+)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <Provider
-        store={createStoreWithMiddleware()}
-    >
+    <Provider store={createStoreWithMiddleware(
+        window.__REDUX_DEVTOOLS_EXTENSION__&&
+        window.__REDUX_DEVTOOLS_EXTENSION__()
+    )}>
         <App />
     </Provider>
 );
