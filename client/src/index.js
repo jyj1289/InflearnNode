92b4fa1441ae15d725895f8b4712eb1817aaf33d
jyj1289 <jin37091289@gmail.com>
2023-08-09 17:52:39 +0900

    SET :: Redux 기본 세팅

--- client/src/index.js
+++ client/src/index.js
@@ -3,14 +3,21 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+import { Provider } from "react-redux";
+import "antd";
+import { applyMiddleware, createStore } from "redux";
+import promiseMiddleware from "redux-promise";
+import ReduxThunk from "redux-thunk";
 
-import "antd/dist/antd.css";
+const createStoreWithMiddleware = applyMiddleware(promiseMiddleware, ReduxThunk)(createStore);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-    <React.StrictMode>
+    <Provider
+        store={createStoreWithMiddleware()}
+    >
         <App />
-    </React.StrictMode>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
